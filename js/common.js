e5aa3677e5e7f96e03bf0a142acd87137047bb3b
Grom <gromimol@ukr.net>
2019-09-29 21:00:42 +0300

    features

--- js/common.js
+++ js/common.js
@@ -42,9 +42,9 @@ $(document).ready(function () {
 
 		var dataTab = $(this).attr('data-tab');
 
-		$('.tab-list a').removeClass('active');
+		$(this).closest('.tab-container').find('.tab-list a').removeClass('active');
 		$(this).addClass('active');
-		$('.tab-content').removeClass('active');
+		$(this).closest('.tab-container').find('.tab-content').removeClass('active');
 		$('#' + dataTab).addClass('active');
 	});
 
@@ -56,5 +56,25 @@ $(document).ready(function () {
 		dots: true,
 		prevArrow: '<span class="prev-arrow"><svg width="16" height="30" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 16 30"><g><g><path d="M1,15l14,14" fill-opacity="0" fill="currentColor" stroke-linejoin="round" stroke-linecap="round" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="2"></path></g><g><path d="M15,1l-14,14" fill-opacity="0" fill="currentColor" stroke-linejoin="round" stroke-linecap="round" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="2"></path></g></g></svg></span>',
 		nextArrow: '<span class="next-arrow"><svg width="16" height="30" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 16 30"><g><g><path d="M15,15l-14,-14" fill-opacity="0" fill="currentColor" stroke-linejoin="round" stroke-linecap="round" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="2"></path></g><g><path d="M1,29l14,-14" fill-opacity="0" fill="currentColor" stroke-linejoin="round" stroke-linecap="round" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="2"></path></g></g></svg></span>'	
+	});
+
+	$('.rates-slider').slick({
+		slidesToShow: 3,
+		variableWidth: true,
+		infinite: false,
+		responsive: [
+		{
+			breakpoint: 990,
+			settings: {
+				slidesToShow: 2
+			}
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1
+			}
+		}
+		]
 	});
 })
