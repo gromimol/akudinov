06b5398f22bd4ba767eae29161df1d1552919536
Grom <gromimol@ukr.net>
2019-09-29 19:19:26 +0300

    top-slider

--- js/common.js
+++ js/common.js
@@ -34,5 +34,27 @@ $(document).ready(function () {
 		e.preventDefault();
 
 		$('.dropmenu').removeClass('visible');
-	})
+	});
+
+	// tabs
+	$('.tab-list').on('click', 'a', function (e) {
+		e.preventDefault();
+
+		var dataTab = $(this).attr('data-tab');
+
+		$('.tab-list a').removeClass('active');
+		$(this).addClass('active');
+		$('.tab-content').removeClass('active');
+		$('#' + dataTab).addClass('active');
+	});
+
+
+	// slider
+	$('.banner-slider').slick({
+		fade: true,
+		appendArrows: '.banner-slider__arrows',
+		dots: true,
+		prevArrow: '<span class="prev-arrow"><svg width="16" height="30" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 16 30"><g><g><path d="M1,15l14,14" fill-opacity="0" fill="currentColor" stroke-linejoin="round" stroke-linecap="round" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="2"></path></g><g><path d="M15,1l-14,14" fill-opacity="0" fill="currentColor" stroke-linejoin="round" stroke-linecap="round" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="2"></path></g></g></svg></span>',
+		nextArrow: '<span class="next-arrow"><svg width="16" height="30" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 16 30"><g><g><path d="M15,15l-14,-14" fill-opacity="0" fill="currentColor" stroke-linejoin="round" stroke-linecap="round" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="2"></path></g><g><path d="M1,29l14,-14" fill-opacity="0" fill="currentColor" stroke-linejoin="round" stroke-linecap="round" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="2"></path></g></g></svg></span>'	
+	});
 })
