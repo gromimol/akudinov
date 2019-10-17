0848a9c4ea96886fcda30c93c3044182f18a47b9
Grom <gromimol@ukr.net>
2019-10-17 14:10:32 +0300

    правки

--- js/common.js
+++ js/common.js
@@ -44,10 +44,12 @@ $(document).ready(function () {
 
 		$(this).closest('.tab-container').find('.tab-list a').removeClass('active');
 		$(this).addClass('active');
-		$(this).closest('.tab-container').find('.tab-content').removeClass('active');
-		$('#' + dataTab).addClass('active');
+		$(this).closest('.tab-container').find('.tab-content').fadeOut();
+		$('#' + dataTab).fadeIn('slow');
 	});
 
+	// Dragscroll
+	
 
 	// slider
 	$('.banner-slider').slick({
@@ -58,83 +60,83 @@ $(document).ready(function () {
 		nextArrow: '<span class="next-arrow"><svg width="16" height="30" xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns:avocode="https://avocode.com/" viewBox="0 0 16 30"><g><g><path d="M15,15l-14,-14" fill-opacity="0" fill="currentColor" stroke-linejoin="round" stroke-linecap="round" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="2"></path></g><g><path d="M1,29l14,-14" fill-opacity="0" fill="currentColor" stroke-linejoin="round" stroke-linecap="round" stroke-opacity="1" stroke="currentColor" stroke-miterlimit="50" stroke-width="2"></path></g></g></svg></span>'	
 	});
 
-	$('.rates-slider').slick({
-		slidesToShow: 3,
-		variableWidth: true,
-		arrows: false,
-		infinite: false,
-		responsive: [
-		{
-			breakpoint: 990,
-			settings: {
-				slidesToShow: 2
-			}
-		},
-		{
-			breakpoint: 480,
-			settings: {
-				slidesToShow: 1
-			}
-		}
-		]
-	});
+	// $('.rates-slider').slick({
+	// 	slidesToShow: 3,
+	// 	variableWidth: true,
+	// 	arrows: false,
+	// 	infinite: false,
+	// 	responsive: [
+	// 	{
+	// 		breakpoint: 990,
+	// 		settings: {
+	// 			slidesToShow: 2
+	// 		}
+	// 	},
+	// 	{
+	// 		breakpoint: 480,
+	// 		settings: {
+	// 			slidesToShow: 1
+	// 		}
+	// 	}
+	// 	]
+	// });
 
-	$('.case-slider').slick({
-		slidesToShow: 2,
-		variableWidth: true,
-		infinite: false,
-		arrows: false,
-		responsive: [
-		{
-			breakpoint: 990,
-			settings: {
-				slidesToShow: 2
-			}
-		},
-		{
-			breakpoint: 480,
-			settings: {
-				slidesToShow: 1
-			}
-		}
-		]
-	});
+	// $('.case-slider').slick({
+	// 	slidesToShow: 2,
+	// 	variableWidth: true,
+	// 	infinite: false,
+	// 	arrows: false,
+	// 	responsive: [
+	// 	{
+	// 		breakpoint: 990,
+	// 		settings: {
+	// 			slidesToShow: 2
+	// 		}
+	// 	},
+	// 	{
+	// 		breakpoint: 480,
+	// 		settings: {
+	// 			slidesToShow: 1
+	// 		}
+	// 	}
+	// 	]
+	// });
 
-	$('.team-slider').slick({
-		slidesToShow: 4,
-		variableWidth: true,
-		infinite: false,
-		arrows: false,
-		responsive: [
-		{
-			breakpoint: 990,
-			settings: {
-				slidesToShow: 3
-			}
-		}
-		]
-	});
+	// $('.team-slider').slick({
+	// 	slidesToShow: 4,
+	// 	variableWidth: true,
+	// 	infinite: false,
+	// 	arrows: false,
+	// 	responsive: [
+	// 	{
+	// 		breakpoint: 990,
+	// 		settings: {
+	// 			slidesToShow: 3
+	// 		}
+	// 	}
+	// 	]
+	// });
 
-	$('.video-slider').slick({
-		slidesToShow: 5,
-		variableWidth: true,
-		infinite: false,
-		arrows: false,
-		responsive: [
-		{
-			breakpoint: 480,
-			settings: {
-				slidesToShow: 2
-			}
-		},
-		{
-			breakpoint: 420,
-			settings: {
-				slidesToShow: 1
-			}
-		}
-		]
-	});
+	// $('.video-slider').slick({
+	// 	slidesToShow: 5,
+	// 	variableWidth: true,
+	// 	infinite: false,
+	// 	arrows: false,
+	// 	responsive: [
+	// 	{
+	// 		breakpoint: 480,
+	// 		settings: {
+	// 			slidesToShow: 2
+	// 		}
+	// 	},
+	// 	{
+	// 		breakpoint: 420,
+	// 		settings: {
+	// 			slidesToShow: 1
+	// 		}
+	// 	}
+	// 	]
+	// });
 
 	$('.document-slider').slick({
 		slidesToShow: 8,
@@ -163,47 +165,47 @@ $(document).ready(function () {
 		]
 	});
 
-	$('.text-review-slider').slick({
-		slidesToShow: 3,
-		variableWidth: true,
-		infinite: false,
-		arrows: false,
-		responsive: [
-		{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 2
-			}
-		},
-		{
-			breakpoint: 420,
-			settings: {
-				slidesToShow: 1
-			}
-		}
-		]
-	});
+	// $('.text-review-slider').slick({
+	// 	slidesToShow: 3,
+	// 	variableWidth: true,
+	// 	infinite: false,
+	// 	arrows: false,
+	// 	responsive: [
+	// 	{
+	// 		breakpoint: 768,
+	// 		settings: {
+	// 			slidesToShow: 2
+	// 		}
+	// 	},
+	// 	{
+	// 		breakpoint: 420,
+	// 		settings: {
+	// 			slidesToShow: 1
+	// 		}
+	// 	}
+	// 	]
+	// });
 
-	$('.faq-slider').slick({
-		slidesToShow: 3,
-		variableWidth: true,
-		infinite: false,
-		arrows: false,
-		responsive: [
-		{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 2
-			}
-		},
-		{
-			breakpoint: 420,
-			settings: {
-				slidesToShow: 1
-			}
-		}
-		]
-	});
+	// $('.faq-slider').slick({
+	// 	slidesToShow: 3,
+	// 	variableWidth: true,
+	// 	infinite: false,
+	// 	arrows: false,
+	// 	responsive: [
+	// 	{
+	// 		breakpoint: 768,
+	// 		settings: {
+	// 			slidesToShow: 2
+	// 		}
+	// 	},
+	// 	{
+	// 		breakpoint: 420,
+	// 		settings: {
+	// 			slidesToShow: 1
+	// 		}
+	// 	}
+	// 	]
+	// });
 
 	$('.video-slider__content').on('click',function (e) {
 		e.preventDefault();
